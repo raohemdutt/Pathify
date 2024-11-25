6fd8853ff1c9dc12064d9f0571a4772876b095c9
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-11-24 23:44:14 -0500

    Added styling for jsx components

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import styles from "../index.css"
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <section class="bg-amber-50 h-[100vh] w-[100%] absolute">
+        <div class="text-black">Home</div>
+        <button className="btn">Daisy UI/Tailwind Button</button>
+    </section>
   )
 }
