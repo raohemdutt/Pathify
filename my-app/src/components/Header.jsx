4212a1373bed5c7159bcfcc9b353690503715c16
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-11-25 00:02:32 -0500

    Added base header styling and icon

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PathifyLogo from '../imgs/pathifyLogo.png'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <section class="flex flex-row justify-between bg-white h-[5.38vw] fixed w-full z-[1] top-[0%] shadow-md">
+      <div class="flex items-center ml-[2%]">
+        <a href='/'>
+          <img class="w-[3vw] h-[3vw] mr-[5px]" src={PathifyLogo}></img>
+        </a>
+        <div class="text-black">Pathify</div>
+      </div>
+    </section>
   )
 }
--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from "../index.css"
+import Search from "./Search"
+import Header from "./Header"
 
 export default function Home() {
   return (
     <section class="bg-amber-50 h-[100vh] w-[100%] absolute">
+        <Header></Header>
+        <Search></Search>
         <div class="text-black">Home</div>
         <button className="btn">Daisy UI/Tailwind Button</button>
     </section>
