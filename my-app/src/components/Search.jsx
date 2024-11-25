2627897b13fdcca76d0d72f44e8ad60f90ebb7a3
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-11-25 18:42:30 -0500

    Added base tailwind and daisy ui theme and go button

--- my-app/src/components/Search.jsx
+++ my-app/src/components/Search.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function Search() {
   return (
-    <section class="flex items-center justify-evenly h-screen">
+    <div class="flex flex-col h-screen">
+      <section class="flex items-center justify-evenly mt-[35vh]">
         <div class="w-[17vw]">
           <label className="form-control w-full max-w-xs">
             <div className="label">
@@ -17,37 +18,41 @@ export default function Search() {
               <option>City 3</option>
             </select>
           </label>
-      </div>
-      <div class="w-[17vw]">
-        <label className="form-control w-full max-w-xs">
-          <div className="label">
-            <span className="label-text">Search By</span>
-            <span className="label-text-alt">Priority</span>
-          </div>
-          <select className="select select-bordered">
-            <option disabled selected>Search Type</option>
-            {/* For loop react component with options, should just pass in I think text to component, create state for this hashmap of text as well */}
-            <option>Price</option>
-            <option>Size</option>
-          </select>
-        </label>
-      </div>
-      {/* Conditionnaly render below based on if user picks sort by price or size */}
-      <div class="w-[17vw]">
-        <label className="form-control w-full max-w-xs">
-          <div className="label">
-            <span className="label-text">Target Price/Size</span>
-            <span className="label-text-alt">$/sq. ft</span>
-          </div>
-          <select className="select select-bordered">
-            <option disabled selected>Pick Target Price</option>
-            {/* For loop react component with options, should just pass in I think text to component, create state for this hashmap of text as well */}
-            <option>$100000</option>
-            <option>$500000</option>
-            <option>%750000</option>
-          </select>
-        </label>
-      </div>
-    </section>
+        </div>
+        <div class="w-[17vw]">
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Search By</span>
+              <span className="label-text-alt">Priority</span>
+            </div>
+            <select className="select select-bordered">
+              <option disabled selected>Search Type</option>
+              {/* For loop react component with options, should just pass in I think text to component, create state for this hashmap of text as well */}
+              <option>Price</option>
+              <option>Size</option>
+            </select>
+          </label>
+        </div>
+        {/* Conditionnaly render below based on if user picks sort by price or size */}
+        <div class="w-[17vw]">
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Target Price/Size</span>
+              <span className="label-text-alt">$/sq. ft</span>
+            </div>
+            <select className="select select-bordered">
+              <option disabled selected>Pick Target Price</option>
+              {/* For loop react component with options, should just pass in I think text to component, create state for this hashmap of text as well */}
+              <option>$100000</option>
+              <option>$500000</option>
+              <option>$750000</option>
+            </select>
+          </label>
+        </div>
+        </section>
+      <section class="flex justify-center mt-[10vh]">
+        <button className="btn btn-outline btn-primary w-[25vw]">Find Your Path -{`>`}</button>
+      </section>
+    </div>
   )
 }
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -2,7 +2,38 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      mytheme: {
+        "primary": "#0d78db",
+        "secondary": "#22c55e",
+        "accent": "#FFFFFF",
+        "neutral": "#000000",
+        "base-100": "#000000"
+      },
+    },
+    colors: {
+      primary: '#0d78db', // Blue
+      primary_daisy: '#377cfb', // Green
+      secondary: '#22c55e', // Green
+      secondary_daisy: '#66cc8a', // Blue
+      exact_secondary: '#85B446',
+      white: '#FFFFFF',
+      opaque_white: "rgb(255 255 255 / 90%)",
+      black: '#000000',
+    },
   },
   plugins: [require('daisyui'),],
+  daisyui: {
+    themes: [
+      {
+      'mytheme': {
+          'primary': '#0e87eb',
+          'secondary': '#22c55e',
+          'accent': '#FFFFFF',
+          'neutral': '#FFFFFF',
+          'base-100': '#ffffff'
+        },
+      },
+    ],
+  },
 }
