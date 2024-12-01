e36a35fa266b00ec873745d887484377209a7815
kaimcfarlane <kaimcfarlane43@gmail.com>
2024-12-01 11:32:54 -0500

    Added basic animation to app

--- my-app/src/components/PathPage.jsx
+++ my-app/src/components/PathPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import MapWithRoute from "./Map.jsx"
 
 export default function PathPage({curLocation}) {
@@ -12,13 +12,31 @@ export default function PathPage({curLocation}) {
     PrevNodes: [[24.5, 34.5],[23.4,34.5],[24.6, 56.4],[34.2,53.6],[35.4, 64.3]]
   });  
 
+  const [curIdx, setCurIdx] = useState(0);
+
+  // Manage interval for updating curIdx
+  useEffect(() => {
+    
+    if (curIdx >= optPropData.PrevNodes.length) return; // Stop interval if all nodes are processed
+
+    const intervalId = setTimeout(() => {
+      setCurIdx((prevIdx) => {
+        if (prevIdx <= optPropData.PrevNodes.length - 1) {
+          return prevIdx + 1;
+        } else {
+          clearInterval(intervalId); // Clear the interval when reaching the end
+          return prevIdx;
+        }
+      });
+    }, 3000);
+
+    return () => clearInterval(intervalId); // Cleanup on component unmount
+  }, [curIdx, optPropData.PrevNodes.length]);
+
   return (
     <>
         <div className="modal-box bg-primary max-w-[100vw] w-[80vw] h-[41vw] 2xl:h-[30.9vw]">
             <div className="card lg:card-side bg-base-100 shadow-xl">
-            {/* <figure>
-                <Image src={glasses_wall_pic} className='w-[35vw] h-[29.03vw]'></Image>
-            </figure> */}
                 <div className="card-body">
                         <div className="modal-action flex flex-col justify-center items-center mt-[0] 2xl:mt-[3vw]">
                             <div class="font-medium">
@@ -28,9 +46,24 @@ export default function PathPage({curLocation}) {
                                 {curLocation.djk === true ? "A-Star's" : "Djkstra's"} Algorithm took {curLocation.djk === true ? optPropData.AStrTime : optPropData.djkTime} seconds
                             </div>
                         </div>
-                        {/* Lopp through all past nodes and insert embed, then isnert embed on last location */}
-                        {/* <iframe width="600" height="450" style={{"border":0}} loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJQatg2-VAXz4RlsWijU8N6EQ&key=AIzaSyAxUna0O0shXH2e2Uz9fLaBTSY8oxIezsM"></iframe> */}
-                        <MapWithRoute latStart={curLocation.lat} longStart={curLocation.long} latEnd={optPropData.lat} longEnd={optPropData.long}></MapWithRoute>
+                        {/* Render MapWithRoute for the current node */}
+                        {curIdx < optPropData.PrevNodes.length && (
+                        <MapWithRoute
+                            latStart={optPropData.PrevNodes[curIdx][0]}
+                            longStart={optPropData.PrevNodes[curIdx][1]}
+                            latEnd={optPropData.lat}
+                            longEnd={optPropData.long}
+                        />
+                        )}
+                        {/* Render final location map once the interval finishes */}
+                        {curIdx === optPropData.PrevNodes.length-1 && (
+                        <MapWithRoute
+                            latStart={curLocation.lat}
+                            longStart={curLocation.long}
+                            latEnd={optPropData.lat}
+                            longEnd={optPropData.long}
+                        />
+                        )}
                 </div>
             </div>
         </div>
